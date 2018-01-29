be24ccab8227940e626be4a65f22b469f1497df1
utkarsh_bajpai <ubajpai1aa@gmail.com>
2018-01-29 23:14:46 +0530

    property binding
    property binding

--- src/app/weather-item/weather-item.component.ts
+++ src/app/weather-item/weather-item.component.ts
@@ -4,7 +4,8 @@ import { WeatherItem } from '../weather-item';
 @Component({
   selector: 'app-weather-item',
   templateUrl: './weather-item.component.html',
-  styleUrls: ['./weather-item.component.css']
+  styleUrls: ['./weather-item.component.css'],
+  inputs : ['weatherItem']
 })
 export class WeatherItemComponent implements OnInit {
 	weatherItem: WeatherItem;
